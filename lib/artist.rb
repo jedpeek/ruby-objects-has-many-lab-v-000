9762126e4e81b76516885c5e462c5bb2f38064ca
Jed Peek <jedpeek@yahoo.com>
2018-06-11 17:40:04 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,8 +6,8 @@ class Artist
     @songs = []
   end
 
-  def add_song(name)
-    song = Song.new(name)
+  def add_song
+    song = Song.new
     @songs << song
     song.artist = self.name
     song
